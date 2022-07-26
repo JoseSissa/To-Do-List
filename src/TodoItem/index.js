452555073147a9cf75e64353c363748a4b80c6cc
Jose <jsissame3484@universidadean.edu.co>
2022-07-26 17:28:14 -0500

    Changes in background

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -5,20 +5,13 @@ import './TodoItem.css';
 
 function TodoItem(props) {
   return (
-    <li className="TodoItem">
-      <CompleteIcon
-        completed={props.completed}
-        onComplete={props.onComplete}
-      />
-      <p
-        className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}
-      >
+    <div className="TodoItem">
+      <CompleteIcon completed={props.completed} onComplete={props.onComplete} />
+      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <DeleteIcon
-        onDelete={props.onDelete}
-      />
-    </li>
+      <DeleteIcon onDelete={props.onDelete}/>
+    </div>
   );
 }
 
